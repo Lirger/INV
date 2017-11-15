d999780f70c21ddfae75578e9426102fbc6d4b4d
lirger <lirger@okm.org.ru>
2017-11-16 01:47:41 +0300

    Upgrade deploy.sh

--- install/deploy.sh
+++ install/deploy.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-sudo apt-get install postgresql -y
-sudo apt-get install lua5.3 -y
+
+source ./settings
+
+function Application_install { 
+	printf "Install $1 "
+	sudo apt-get install $1 -y >> $LOG_FILE 2>&1 
+	if [ $? -eq 0 ]
+	then
+		printf "%-20s [OK]\n"
+	else           
+		printf "%-20s [FAIL]\n"
+	fi
+}
+
+
+## MAIN
+rm -rf -R -f $LOG_DIR
+mkdir $LOG_DIR 
+
+Application_install postgresql 
+Application_install sqitch
+Application_install lua5.3 
